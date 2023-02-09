2de47011d4289143ee3dbf32c519daeaf9f6011d
toniroigchulia <aroigchulia@cifpfbmoll.eu>
2023-02-09 16:23:04 +0100

    javaproject

--- Programacio/Java/PolideportivoNatacion/PolideportivoNatacion/src/App.java
+++ Programacio/Java/PolideportivoNatacion/PolideportivoNatacion/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        int[][] Horas = {{15,19,20,12},{16,14,12,11},{17,20,11,10},{11,12,13,18},{18,19,20,19}};
+        System.out.println(Horas[0][]);
     }
 }
