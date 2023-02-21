7c2053c7cb9a85c8023daf2ab34e6aebce2ecf60
Toni <aroigchulia@cifpfbmoll.eu>
2023-02-21 20:46:21 +0100

    pythonhtml

--- Llenguatge de Marques/Exercici 13/customWeb.py
+++ Llenguatge de Marques/Exercici 13/customWeb.py
@@ -1,64 +1,109 @@
 import webbrowser
+import os
 
-my_file = open("web.txt")
+background_color_primary = input("Color primario de la tabla: ")
+background_color_secondary = input("Color secundario de la tabla: ")
 
-index = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd%22%3E
-<html xmlns="http://www.w3.org/1999/xhtml%22%3E
+jugadores = []
+puntuacion = []
+
+#Añadir informacion a las listas
+num_jugadores = int(input("\n","Cuantos jugadores quieres añadir? "))
+while num_jugadores > 0:
+  os.system("cls")
+  
+  print("Color primaro: "+background_color_primary)
+  print("Color secundario: " + background_color_secondary+"\n")
+  
+  print(jugadores)
+  print(puntuacion,"\n")
+  
+  print("Jugadores restantes: ",num_jugadores)
+  jugadores.append(input("Jugador: "))
+  puntuacion.append(input("Puntuacion: "))
+  print("\n")
+  
+  num_jugadores -= 1
+
+#Abrimos los documentos para editarlos
+html_file = open("web.html", "w")
+css_file = open("estilos.css", "w")
+
+#Principio de html
+html = """
+<!DOCTYPE html>
+<html>
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <link rel="stylesheet" type="text/css" href="estilos.css" title="style" />
-<style type="text/css">
 
+<style type="text/css">
 </style>
+
 </head>
 <body>
 
+  <table>
+    <tr>
+      <th>Posicion</th>
+      <th>Jugador</th>
+      <th>Puntos</th>
+    </tr>
+"""
 
-    <table>
-        <thead>
-          <tr>
-            <th>Posicion</th>
-            <th>Jugador</th>
-            <th>Puntos</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Miquel</td>
-            <td>1000</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jumi</td>
-            <td>18</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Maximo</td>
-            <td>14</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Isaac</td>
-            <td>13</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>Messi</td>
-            <td>12</td>
-          </tr>
-        </tbody>
-      </table>
+#Añadimos una tupla por cada jugador en la array de jugadores
+for x in range(len(jugadores)):
 
+  html += f"""
+      <tr>
+        <td>{(x+1)}</td>
+        <td>{jugadores[x]}</td>
+        <td>{puntuacion[x]}</td>
+      </tr>
+  """
 
+#Final de html
+html += """
+  </table>  
 </body>
 </html>
 """
 
-my_file.write(index)
+#CSS
+css = f"""
 
-my_file.close()
+table {{
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }}
+  
+  table td, table th {{
+    border: 1px solid #ddd;
+    padding: 8px;
+  }}
+  
+  table tr:nth-child(even){{background-color: {background_color_secondary};}}
+  
+  table tr:hover {{background-color: #ddd;}}
+  
+  table th {{
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: {background_color_primary};
+    color: white;
+  }}
 
-webbrowser.open_new_tab("web.txt")
+"""
+
+#Escribimos los documentos con la informacion correspondiente
+html_file.write(html)
+css_file.write(css)
+
+#Cerramos los archivos
+html_file.close()
+css_file.close()
+
+#Abrimos la pestaña
+webbrowser.open_new_tab("web.html")
