f15c2ce9ca66d23ff1203088c757203829422706
toniroigchulia <aroigchulia@cifpfbmoll.eu>
2023-04-03 18:16:02 +0200

    recursividad

---
+++ Programacio/Python/Recursividad/Turtle/Sierpnski/sierpnski.py
@@ -0,0 +1,30 @@
+from turtle import *
+
+def sierpinski(level, size):
+    
+    if (level == 0):
+        return
+    
+    sierpinski(level-1,size/2)
+    forward(size)
+    right(120)
+    
+    sierpinski(level-1,size/2)
+    forward(size)
+    right(120)
+    
+    sierpinski(level-1,size/2)
+    forward(size)
+    right(120)
+     
+     
+speed(0)
+penup()
+goto(-300, 200)
+pendown()
+level = 3
+size = 300
+    
+sierpinski(level, size)
+    
+exitonclick()
