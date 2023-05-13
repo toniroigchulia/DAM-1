b5d2d4c1c31ae45e76e9ba219720ae1a3234f653
Toni <aroigchulia@cifpfbmoll.eu>
2023-05-13 12:41:46 +0200

    tomcat, eclipse

--- ProjecteFinalEntorns/Frontend/Login/login.js
+++ ProjecteFinalEntorns/Frontend/Login/login.js
@@ -1,6 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    
+    document.getElementById("botonEnviar").addEventListener("click", send);
+
+
+});
+
 function send(){
+    var ehttp = new XMLHttpRequest();
     
-    location.href = "../Gestio/gestio.html"
+    ehttp.open("POST", "http://172.16.138.129:3000/OperationDam/Suma", true);
+    ehttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    ehttp.send("a="+document.getElementById("num1").value+"&b="+document.getElementById("num2").value);
     
-}
-    
+    ehttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("num1").value = "";
+            document.getElementById("num2").value = "";
+            document.getElementById("resultado").innerHTML = ehttp.responseText;
+        }
+    };
+};    
